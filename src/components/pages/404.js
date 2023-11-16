74a8e0a6ab270dba77259926c4243f1b49d81ac1
ElSemki <alexandr-alexeev5452@rambler.ru>
2023-11-16 14:20:17 +0300

    SEO

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,14 @@
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 import ErrorMessage from '../errorMessage/errorMessage';
 
 const Page404 = () => {
 	return (
 		<div>
+			<Helmet>
+				<meta name="description" content="Error page" />
+				<title>Marvel information: 404</title>
+			</Helmet>
 			<ErrorMessage />
 			<p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>
 				Page doesn't exist
--- src/components/pages/singleCharLayout/SingleCharLayout.js
+++ src/components/pages/singleCharLayout/SingleCharLayout.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import './singleChar.scss';
 
 const SingleCharLayout = ({ data }) => {
@@ -5,6 +6,10 @@ const SingleCharLayout = ({ data }) => {
 
 	return (
 		<div className="single-char">
+			<Helmet>
+				<meta name="description" content={`${name} character`} />
+				<title>{name}</title>
+			</Helmet>
 			<img className="single-char__picture" src={thumbnail} alt={name} />
 			<div>
 				<h2 className="single-char__name">{name}</h2>
